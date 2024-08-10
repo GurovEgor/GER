b8f81669537cf7a9423e3c75a8a5500456e2bb79
GOGA <79186561976@yandex.ru>
2024-08-10 20:05:57 +0300

    бебра найдена

---
+++ XYphp.php
@@ -0,0 +1,45 @@
+<?php
+$array = [[' . ',' . ',' . '],[' . ',' . ',' . '],[' . ',' . ',' . ']];
+foreach ($array as $row){
+           $sep='|';
+          echo '|'.implode($sep, $row).'|'.PHP_EOL;
+    
+}
+//ne bebra
+$user=readline("Ход Х, введите кординаты в формате: 1.1 - " );
+
+//echo $user;
+$cordination=explode('.' , $user);
+$y=--$cordination[0];
+$x=--$cordination[1];
+//echo'X= '.$x.PHP_EOL.'Y= '.$y;
+if ($array[$x][$y]==' . '){
+    $array[$x][$y]=' X ';
+}
+else{
+echo 'Место ('.$array[$x][$y].') занято, выберите другое'.PHP_EOL;
+}
+foreach ($array as $row){
+    $sep='|';
+   echo '|'.implode($sep, $row).'|'.PHP_EOL;
+
+}
+
+$user=readline("Ход O, введите кординаты в формате: 1.1 - " );
+
+//echo $user;
+$cordination=explode('.' , $user);
+$y=$cordination[0]-1;
+$x=$cordination[1]-1;
+//echo'X= '.$x.PHP_EOL.'Y= '.$y;
+if ($array[$x][$y]==' . '){
+    $array[$x][$y]=' O ';}
+else{
+echo 'Место ('.$array[$x][$y].') занято, выберите другое'.PHP_EOL;
+}
+foreach ($array as $row){
+    $sep='|';
+   echo '|'.implode($sep, $row).'|'.PHP_EOL;
+
+}
+?> 
